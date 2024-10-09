49a752fa7e16bcfc29693cb32699afb28081b876
SaurabhApraj <saurabhapraj7@gmail.com>
2024-10-09 21:42:14 +0530

    vercel setting

--- library_system_backend/settings.py
+++ library_system_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",".vercel.app",".now.sh"]
 
 
 # Application definition
@@ -151,3 +151,6 @@ SPECTACULAR_SETTINGS = {
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
